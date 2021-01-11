90487acfef6d3c6b7e12c88443d364bf3bc24c35
jklmnya <39192356+jklmnya@users.noreply.github.com>
2021-01-11 21:54:37 +0800

    Create 104.cpp

---
+++ ACwing/104.cpp
@@ -0,0 +1,29 @@
+/**
+* 在一条数轴上有N家商店，它们的坐标分别为 A1~AN。
+* 现在需要在数轴上建立一家货仓，每天清晨，从货仓到每家商店都要运送一车商品。
+* 为了提高效率，求把货仓建在何处，可以使得货仓到每家商店的距离之和最小。
+* 
+* 贪心，排序后取中位数（最佳位置）
+**/
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 100010;
+
+int n;
+int a[N];
+
+int main() {
+    cin >> n;
+    for (int i = 0;i<n;i++) { cin >> a[i]; }
+    sort(a, a + n);
+    int res = 0;
+    for (int i = 0;i<n;i++) {
+        res += abs(a[i] - a[n / 2]);
+    }
+    cout << res <<endl;
+    return 0;
+}
