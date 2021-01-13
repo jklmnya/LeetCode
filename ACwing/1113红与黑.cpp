2a1d8e204ee888b61e74ea63d4310c07a3e4b9cf
jklmnya <39192356+jklmnya@users.noreply.github.com>
2021-01-13 18:33:57 +0800

    Update 1113红与黑.cpp

--- ACwing/1113红与黑.cpp
+++ ACwing/1113红与黑.cpp
@@ -55,3 +55,48 @@ int main() {
     }
     return 0;
 }
+
+/**
+ * dfs：深度优先
+ **/
+ 
+#include <iostream>
+
+using namespace std;
+
+const int N = 22;
+
+int n, m;
+char f[N][N];
+
+int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
+
+int dfs(int x, int y) {
+    int res = 1;
+    f[x][y] = '#';  //设置当前坐标已被访问
+    for (int i = 0;i<4;i++) {
+        int a = x + dx[i], b = y + dy[i];
+        // 判断下一个坐标是否可以被访问
+        if (x >= 0 && x < m && y >= 0 && y < n && f[a][b] == '.') {
+            res += dfs(a, b);
+        }
+    }
+    return res;
+}
+
+int main() {
+    while (cin >> n >> m, n || m) {
+        int x, y;
+        for (int i = 0;i<m;i++) {
+            for (int j = 0;j<n;j++) {
+                cin >> f[i][j];
+                if (f[i][j] == '@') {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        cout << dfs(x, y) << endl;
+    }
+    return 0;
+}
